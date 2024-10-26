ae01d880ef44afb76bb8aa25f662fa580017ddc0
AhmedFatrah2001 <fatrah.ahmed@gmail.com>
2024-10-27 00:15:57 +0100

    fixed

---
+++ src/main/java/org/example/booksfrog/BooksfrogApplication.java
@@ -0,0 +1,13 @@
+package org.example.booksfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksfrogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksfrogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/booksfrog/BooksfrogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booksfrog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksfrogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
